0fba3276a943bbd95428fb79cd532f14b760709a
Sohib-ezaldeen <sohibezladeengithub@gmail.com>
2026-03-09 22:15:19 +0200

    frist commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector(".btn-share");
+ 
+ let boxShare = document.querySelector(".share-box"); 
+btn.addEventListener("click",()=>{
+ boxShare.classList.toggle("none")
+})
